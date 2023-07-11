43b5035e7b12c49ad801f940c18f7e6e01afae46
Rohith M <rohith.m068@gmail.com>
2023-07-11 12:14:19 +0530

    Creating VPC Peering Connection

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
+#  subnets = each.value["subnets"]
   env = var.env
   tags = var.tags
   default_vpc_id = var.default_vpc_id
