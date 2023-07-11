6e8c9797fae22f09be898b06437cc9c35b18eb30
Rohith M <rohith.m068@gmail.com>
2023-07-11 12:09:36 +0530

    Creating VPC Peering Connection

--- main.tf
+++ main.tf
@@ -16,3 +16,16 @@ module "vpc" {
   tags = var.tags
   default_vpc_id = var.default_vpc_id
 }
+
+
+output "subnet_ids" {
+  value = module.vpc
+}
+
+#module "app_server" {
+#  component= "test"
+#  source = "git::https://github.com/MROHITH068/terraform-module-app.git"
+#  tags= var.tags
+#  env = var.env
+#  subnet_id =
+#}
