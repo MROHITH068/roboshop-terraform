fd5b4028d3a01c7f4e2c643dff10c114d83f1afa
Rohith M <rohith.m068@gmail.com>
2023-07-13 16:05:01 +0530

    Added RDS cluster

--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "default_vpc_rt" {}
 variable "rabbitmq" {}
 variable "allow_ssh_cidr" {}
 variable "zone_id" {}
+
+variable "database_name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "rds" {}
