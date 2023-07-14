51453acf8ffa72acede24fd867a983bfb182dd81
Rohith M <rohith.m068@gmail.com>
2023-07-14 23:49:43 +0530

    Adding docdb and elasticache subnet groups

--- variables.tf
+++ variables.tf
@@ -8,11 +8,10 @@ variable "default_vpc_rt" {}
 variable "rabbitmq" {}
 variable "allow_ssh_cidr" {}
 variable "zone_id" {}
-variable "kms_key_id" {}
 
 variable "rds" {}
 variable "kms_key_arn" {}
+variable "kms_key_id" {}
 
 variable "documentdb" {}
-
 variable "elasticache" {}
