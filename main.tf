744f063ddf4031cb677f9164a4f578771f6b9230
Rohith M <rohith.m068@gmail.com>
2023-07-15 16:14:52 +0530

    Adding ALB (applicaiton load balancer)

--- main.tf
+++ main.tf
@@ -105,19 +105,19 @@ module "vpc" {
 
 module "alb" {
   source = "git::https://github.com/MROHITH068/terraform-module-alb.git"
-  for_each = var.alb
-  component = each.value["component"]
-  name = each.value["name"]
-  internal = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
-
-  sg_subnet_cidr = each.value["name"]=="public"? ["0.0.0.0/0"] : local.app_web_subnet_cidr
-
-  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),each.value["subnet_ref"],null),"subnet_ids",null)
-
-  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
-
-  tags = var.tags
-  env = var.env
+#  for_each = var.alb
+#  component = each.value["component"]
+#  name = each.value["name"]
+#  internal = each.value["internal"]
+#  load_balancer_type = each.value["load_balancer_type"]
+#
+#  sg_subnet_cidr = each.value["name"]=="public"? ["0.0.0.0/0"] : local.app_web_subnet_cidr
+#
+#  subnets = lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),each.value["subnet_ref"],null),"subnet_ids",null)
+#
+#  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
+#
+#  tags = var.tags
+#  env = var.env
 }
 
