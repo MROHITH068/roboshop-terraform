0dddfa55c350d28e11551589a31066c107c50f01
Rohith M <rohith.m068@gmail.com>
2023-07-15 17:04:40 +0530

    Adding ALB (applicaiton load balancer)

--- main.tf
+++ main.tf
@@ -106,12 +106,12 @@ module "alb" {
   source = "git::https://github.com/MROHITH068/terraform-module-alb.git"
 
   for_each           = var.alb
-  name               = each.value["name"]
-  internal           = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
+#  name               = each.value["name"]
+#  internal           = each.value["internal"]
+#  load_balancer_type = each.value["load_balancer_type"]
   vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
-  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+#  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
 
   env  = var.env
   tags = var.tags
