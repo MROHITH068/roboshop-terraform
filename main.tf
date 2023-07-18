14358a9e00f9418c12e40408c32637ea6461017a
Rohith M <rohith.m068@gmail.com>
2023-07-18 16:17:11 +0530

    Added certificates to aws_lb_listener with ACM to get HTTPS

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ module "documentdb" {
   instance_class = each.value["instance_class"]
 
 
-subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
+  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
   sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
   vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
 
