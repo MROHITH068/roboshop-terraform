8f1eadf76a415ede780470c53c06df7358acc739
Rohith M <rohith.m068@gmail.com>
2023-07-18 14:39:21 +0530

    Creating all the componenets with private subnets

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -133,6 +133,61 @@ apps = {
     min_size = 1
     subnet_ref = "app"
     lb_ref = "private"
-    lb_rule_priority = 100
+    lb_rule_priority = 101
+  }
+  catalogue ={
+    component = "catalogue"
+    app_port = 8080
+    instance_type = "t3.small"
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 102
+  }
+  shipping ={
+    component = "shipping"
+    app_port = 8080
+    instance_type = "t3.small"
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 103
+  }
+  payment ={
+    component = "payment"
+    app_port = 8080
+    instance_type = "t3.small"
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 104
+  }
+  user ={
+    component = "user"
+    app_port = 8080
+    instance_type = "t3.small"
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+    subnet_ref = "app"
+    lb_ref = "private"
+    lb_rule_priority = 105
+  }
+  frontend ={
+    component = "frontend"
+    app_port = 8080
+    instance_type = "t3.small"
+    desired_capacity = 1
+    max_size = 1
+    min_size = 1
+    subnet_ref = "web"
+    lb_ref = "public"
+    lb_rule_priority = 106
   }
 }
--- main.tf
+++ main.tf
@@ -28,79 +28,79 @@ module "vpc" {
 #  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"app",null),"subnet_ids",null)[0]
 #}
 #
-#module "rabbitmq"{
-#  source = "git::https://github.com/MROHITH068/terraform-module-rabbitmq.git"
-#  for_each = var.rabbitmq
-#  component = each.value["component"]
-#  instance_type = each.value["instance_type"]
-#
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
-#  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
-#  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)[0]
-#
-#  tags= var.tags
-#  env = var.env
-#  zone_id = var.zone_id
-#  allow_ssh_cidr = var.allow_ssh_cidr
-#  kms_key_id = var.kms_key_id
-#}
-#
-#module "rds" {
-#  source = "git::https://github.com/MROHITH068/terraform-module-rds.git"
-#  for_each = var.rds
-#  component = each.value["component"]
-#  database_name = each.value["database_name"]
-#  engine = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  instance_class = each.value["instance_class"]
-#  instance_count = each.value["instance_count"]
-#
-#  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
-#  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
-#
-#  kms_key_arn = var.kms_key_arn
-#  tags = var.tags
-#  env = var.env
-#}
-#
-#module "documentdb" {
-#  source = "git::https://github.com/MROHITH068/terraform-module-documentdb.git"
-#  for_each = var.documentdb
-#  component = each.value["component"]
-#  engine = each.value["engine"]
-#  engine_version = each.value ["engine_version"]
-#  db_instance_count = each.value["db_instance_count"]
-#  instance_class = each.value["instance_class"]
-#
-#
-#subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
-#  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
-#
-#  kms_key_arn = var.kms_key_arn
-#  tags = var.tags
-#  env = var.env
-#}
-#
-#module "elasticache" {
-#  source = "git::https://github.com/MROHITH068/terraform-module-elasticache.git"
-#  for_each = var.elasticache
-#  component = each.value["component"]
-#  node_type = each.value["node_type"]
-#  replicas_per_node_group = each.value["replicas_per_node_group"]
-#  num_node_groups = each.value["num_node_groups"]
-#  engine = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#
-#  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
-#  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
-#
-#  kms_key_arn = var.kms_key_arn
-#  tags = var.tags
-#  env = var.env
-#}
+module "rabbitmq"{
+  source = "git::https://github.com/MROHITH068/terraform-module-rabbitmq.git"
+  for_each = var.rabbitmq
+  component = each.value["component"]
+  instance_type = each.value["instance_type"]
+
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
+  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)[0]
+
+  tags= var.tags
+  env = var.env
+  zone_id = var.zone_id
+  allow_ssh_cidr = var.allow_ssh_cidr
+  kms_key_id = var.kms_key_id
+}
+
+module "rds" {
+  source = "git::https://github.com/MROHITH068/terraform-module-rds.git"
+  for_each = var.rds
+  component = each.value["component"]
+  database_name = each.value["database_name"]
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  instance_class = each.value["instance_class"]
+  instance_count = each.value["instance_count"]
+
+  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
+  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
+
+  kms_key_arn = var.kms_key_arn
+  tags = var.tags
+  env = var.env
+}
+
+module "documentdb" {
+  source = "git::https://github.com/MROHITH068/terraform-module-documentdb.git"
+  for_each = var.documentdb
+  component = each.value["component"]
+  engine = each.value["engine"]
+  engine_version = each.value ["engine_version"]
+  db_instance_count = each.value["db_instance_count"]
+  instance_class = each.value["instance_class"]
+
+
+subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
+  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
+
+  kms_key_arn = var.kms_key_arn
+  tags = var.tags
+  env = var.env
+}
+
+module "elasticache" {
+  source = "git::https://github.com/MROHITH068/terraform-module-elasticache.git"
+  for_each = var.elasticache
+  component = each.value["component"]
+  node_type = each.value["node_type"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+  num_node_groups = each.value["num_node_groups"]
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+
+  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main", null ),"subnet_ids",null),"db",null),"subnet_ids",null)
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null ),"subnets",null),"app",null),"cidr_block",null)
+  vpc_id= lookup(lookup(module.vpc, "main", null),"vpc_id",null)
+
+  kms_key_arn = var.kms_key_arn
+  tags = var.tags
+  env = var.env
+}
 
 module "alb" {
   source = "git::https://github.com/MROHITH068/terraform-module-alb.git"
